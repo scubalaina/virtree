7f396e01c77ed284f498f42d357e36d9b5f213ed
Alaina Weinheimer <alainarw94@gmail.com>
2025-04-25 11:58:25 -0400

    Update virtree.py

--- virtree.py
+++ virtree.py
@@ -5,13 +5,14 @@ from collections import defaultdict
 #hmmdb = "hmm/vog_05p.hmm"
 
 # run HMMER3
-def run_hmmer(input_file, cpus, evalue, database):
+def run_hmmer(input_file, cpus, evalue, database, redo):
 	output_file = re.sub(".faa", ".hmmout", input_file)
 	cmd = "hmmsearch -E "+ str(evalue) +" --cpu "+ cpus +" --tblout "+ output_file +" "+ database +" "+ input_file	
-	print(cmd)
+	#print(cmd)
 	cmd2 = shlex.split(cmd)
-	subprocess.call(cmd2, stdout=open("out.txt", "w"), stderr=open("err.txt", "w"))
-	os.remove("out.txt")
+	if not redo:
+		subprocess.call(cmd2, stdout=open("out.txt", "w"), stderr=open("err.txt", "w"))
+		os.remove("out.txt")
 	return output_file
 
 # define function for parsing HMMER3 output
@@ -58,7 +59,7 @@ def run_program(inputdir, project, database, minhit, evalue, cpus, redo):
 			inputfile = os.path.join(inputdir, i)
 			#print(inputfile)
 			#protein_file = predict_proteins(inputfile, inputdir)
-			hmmout = run_hmmer(inputfile, cpus, evalue, database)
+			hmmout = run_hmmer(inputfile, cpus, evalue, database, redo)
 			hit_dict = parse_hmmout(hmmout)
 		#	fulllist = i.split(".")
 		#	genomelist = fulllist[0:-2]
@@ -71,6 +72,21 @@ def run_program(inputdir, project, database, minhit, evalue, cpus, redo):
 	df = df.fillna(0)
 	df2 = df.clip(0,1)
 
+	rows_all_one = (df2==1).all(axis=1)
+	#rows_to_drop = rows_all_one.tolist()
+	#rows = [ind if j==1 in enumerate(rows_all_one.tolist()
+	rows = [pair for pair in enumerate(rows_all_one.tolist()) if pair[1] == 1]
+	rows_to_drop = [n[0] for n in rows]
+	#print(rows_to_drop)
+	#print(df2.shape)
+	print("Removed the following VOGs because they were present in all genomes analyzed: ", list(df2.index[rows_to_drop]))
+	if len(rows_to_drop) > 0:
+		df2.drop(df2.index[rows_to_drop], inplace=True)
+		#df2.drop(index=rows_to_drop, axis=1)
+	else:
+		pass
+
+	#print(df2.shape)
 	prof_tbl = project + "_profile.tsv"
 	df2.to_csv(prof_tbl,sep="\t")
 	#if BINARY == 1:
@@ -98,7 +114,7 @@ def main(argv=None):
 	args_parser.add_argument('-p', '--project', required=True, help='project name for outputs')
 	args_parser.add_argument('-db', '--database', required=False, default="hmm/vog_05p.hmm", help='PATH to HMM database to use. Default is hmm/vog_05p.hmm (default), vog_025p.hmm, and vog_005p.hmm. See README for details')
 	args_parser.add_argument('-g', '--minhit', required=False, default=int(1), help='minimum number of VOG hits that each viral region must have to be reported (default=4)')
-	args_parser.add_argument('-e', '--evalue', required=False, default=str(1e-3), help='e-value that is passed to HMMER3 for the VOG hmmsearch (default=1e-10)')
+	args_parser.add_argument('-e', '--evalue', required=False, default=str(1e-3), help='e-value that is passed to HMMER3 for the VOG hmmsearch (default=1e-3)')
 	args_parser.add_argument('-t', '--cpus', required=False, default=str(1), help='number of cpus to use for the HMMER3 search')
 	args_parser.add_argument('-r', '--redo', type=bool, default=False, const=True, nargs='?', help='run without re-launching prodigal and HMMER3 (for quickly re-calculating outputs with different parameters if you have already run once)')
 	args_parser.add_argument('-v', '--version', action='version', version='ViralRecall v. 2.1')
